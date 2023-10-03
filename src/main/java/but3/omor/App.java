8a3243188b8efb91136d95311f429b49e35b2d8f
oskar.morel <oskar.morel@iut-rodez.fr>
2023-10-03 17:31:26 +0200

    Initial Commit

---
+++ src/main/java/but3/omor/App.java
@@ -0,0 +1,12 @@
+package but3.omor;
+
+/**
+ * Hello world!
+ *
+ */
+public class App {
+    public static void main( String[] args )
+    {
+        System.out.println( "Hello World!" );
+    }
+}
---
+++ src/test/java/but3/omor/AppTest.java
@@ -0,0 +1,19 @@
+package but3.omor;
+
+
+import org.junit.jupiter.api.Test;
+
+import static org.junit.jupiter.api.Assertions.assertTrue;
+
+/**
+ * Unit test for simple App.
+ */
+public class AppTest {
+
+    /**
+     * Rigorous Test :-)
+     */
+    @Test
+    public void shouldAnswerWithTrue() { assertTrue( true );}
+}
+
