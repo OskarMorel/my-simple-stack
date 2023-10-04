aa2cc2526f7e18aa0ca077e45d905a3ecad6c05a
oskar.morel <oskar.morel@iut-rodez.fr>
2023-10-04 09:45:49 +0200

    Correction erreurs

--- src/main/java/but3/omor/App.java
+++ src/main/java/but3/omor/App.java
@@ -4,9 +4,12 @@ package but3.omor;
  * Hello world!
  *
  */
-public class App {
-    public static void main( String[] args )
-    {
-        System.out.println( "Hello World!" );
+class App {
+    /**
+     * Programme principal
+     * @param args
+     */
+    public static void main(final String[] args) {
+        System.out.println("Hello World!");
     }
 }
